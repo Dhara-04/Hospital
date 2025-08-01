6834fdbca31315b11868aa10d92f52afd547a9a3
Dhara-04 <dharasavaliya06@gmail.com>
2025-08-01 17:14:52 +0530

    Add files via upload

---
+++ js/style.js
@@ -0,0 +1,38 @@
+$(document).ready(function(){
+    $("#banner").owlCarousel({
+        items: 1,
+        nav: false,
+        loop: true,
+        dots: true,
+        autoplay: true,
+        autoplayTimeout: 5000
+    });
+    $("#D_slider").owlCarousel({
+        items: 1,
+        nav: true,
+        navText: ['<i class="fa-solid fa-angle-left"></i>','<i class="fa-solid fa-angxle-right"></i>'],
+        loop: true,
+        dots: true
+    });
+
+    // loader
+    setTimeout(function(){
+        $('.loader').fadeOut();
+    },3000)
+
+    // bottom to top arrow
+    $(document).ready(function(){
+        $(window).scroll(function(){
+            if ($(this).scrollTop() > 100) {
+                $('.bottom_arrow').fadeIn();
+            } else {
+                $('.bottom_arrow').fadeOut();
+            }
+        });
+        $('.bottom_arrow').click(function(){
+            $('html, body').animate({scrollTop : 0},800);
+            return false;
+        });
+        
+    });
+});
